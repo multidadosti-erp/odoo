ac802dc0eb20c3baad16141c5c6770a10b42f152
Augusto Costa <gustotc@gmail.com>
2020-09-02 17:40:46 -0300

    [IMP] Adiciona IF = 'product' para Regra de Stock modulo 'stock'
    - Adicionado if product_id.type == 'product' para verificação da regra
      do Estoque (Compra ou Transferência)
    
      Obs.: Isso porque o type = 'consu' não controla saldo de Estoque, então
            não precisa verificar a regra.

--- addons/stock/models/stock_rule.py
+++ addons/stock/models/stock_rule.py
@@ -301,17 +301,20 @@ class ProcurementGroup(models.Model):
         In order to be able to find a suitable location that provide the product
         it will search among stock.rule.
         """
-        values.setdefault('company_id', self.env['res.company']._company_default_get('procurement.group'))
-        values.setdefault('priority', '1')
-        values.setdefault('date_planned', fields.Datetime.now())
-        rule = self._get_rule(product_id, location_id, values)
-        if not rule:
-            raise UserError(_('No procurement rule found in location "%s" for product "%s".\n Check routes configuration.') % (location_id.display_name, product_id.display_name))
-        action = 'pull' if rule.action == 'pull_push' else rule.action
-        if hasattr(rule, '_run_%s' % action):
-            getattr(rule, '_run_%s' % action)(product_id, product_qty, product_uom, location_id, name, origin, values)
-        else:
-            _logger.error("The method _run_%s doesn't exist on the procument rules" % action)
+        # Multidados: Adicionado if product_id.type == 'product'
+        # Pois somente Product controla Estoque e Contém Regra de Estoque
+        if product_id.type == 'product':
+            values.setdefault('company_id', self.env['res.company']._company_default_get('procurement.group'))
+            values.setdefault('priority', '1')
+            values.setdefault('date_planned', fields.Datetime.now())
+            rule = self._get_rule(product_id, location_id, values)
+            if not rule:
+                raise UserError(_('No procurement rule found in location "%s" for product "%s".\n Check routes configuration.') % (location_id.display_name, product_id.display_name))
+            action = 'pull' if rule.action == 'pull_push' else rule.action
+            if hasattr(rule, '_run_%s' % action):
+                getattr(rule, '_run_%s' % action)(product_id, product_qty, product_uom, location_id, name, origin, values)
+            else:
+                _logger.error("The method _run_%s doesn't exist on the procument rules" % action)
         return True
 
     @api.model
