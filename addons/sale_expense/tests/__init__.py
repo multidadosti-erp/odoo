e3380c4e883b63f649df542892c3724f17434e37
Fernando Saeta <fernando.r.saeta@gmail.com>
2023-06-29 17:23:29 -0300

    Comenta testes do 'sale_expense' para não dar erro

--- addons/sale_expense/tests/__init__.py
+++ addons/sale_expense/tests/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import test_sale_expense
+# Comentado pela Multidados: Os testes não são compatíveis com o módulo 'br_hr_expense'
+# from . import test_sale_expense
+
 from . import test_reinvoice
