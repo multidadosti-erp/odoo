54ec06f5cbd7d258e13854b570a7cdfd0622bb5b
Augusto Costa <gustotc@gmail.com>
2024-08-15 16:25:04 -0300

    Adiciona Valor sem Impostos as Parcelas módulo 'account'
    - Objetivo é ter o valor bruto, para cálculos gerais sem considerar
      os impostos embutidos.

--- addons/account/models/account_invoice.py
+++ addons/account/models/account_invoice.py
@@ -1191,7 +1191,7 @@ class AccountInvoice(models.Model):
                 'invoice_id': self.id,
                 'currency_id': self.currency_id.id,
             }
-            
+
             res.append(move_line_dict)
 
         return res
@@ -2078,45 +2078,85 @@ class AccountPaymentTerm(models.Model):
             raise ValidationError(_('A Payment Term should have only one line of type Balance.'))
 
     @api.one
-    def compute(self, value, date_ref=False):
+    def compute(self, value, date_ref=False, value_without_taxes=0.0):
+        """Método para Calcular as Parcelas
+
+        Args:
+            value (float): Valor a Calcular as Parcelas
+            date_ref (Date, optional): Data de Referencias para Parcelas. Defaults to False.
+            value_taxes (float, optional): Valor sem Impostos (bruto) a Calcular as Parcelas. Defaults to 0.
+
+        Returns:
+            list: Lista das Parcelas
+        """
         date_ref = date_ref or fields.Date.today()
         amount = value
+        amount_without_taxes = value_without_taxes
         sign = value < 0 and -1 or 1
+        sign_without_taxes = value_without_taxes < 0 and -1 or 1
         result = []
-        if self.env.context.get('currency_id'):
-            currency = self.env['res.currency'].browse(self.env.context['currency_id'])
+
+        if self.env.context.get("currency_id"):
+            currency = self.env["res.currency"].browse(self.env.context["currency_id"])
         else:
             currency = self.env.user.company_id.currency_id
+
         for line in self.line_ids:
-            if line.value == 'fixed':
+            if line.value == "fixed":
                 amt = sign * currency.round(line.value_amount)
-            elif line.value == 'percent':
+                amt_without_taxes = sign_without_taxes * currency.round(line.value_amount)
+            elif line.value == "percent":
                 amt = currency.round(value * (line.value_amount / 100.0))
-            elif line.value == 'balance':
+                amt_without_taxes = currency.round(value_without_taxes * (line.value_amount / 100.0))
+            elif line.value == "balance":
                 amt = currency.round(amount)
+                amt_without_taxes = currency.round(amount_without_taxes)
+
             if amt:
                 next_date = fields.Date.from_string(date_ref)
-                if line.option == 'day_after_invoice_date':
+                if line.option == "day_after_invoice_date":
                     next_date += relativedelta(days=line.days)
                     if line.day_of_the_month > 0:
-                        months_delta = (line.day_of_the_month < next_date.day) and 1 or 0
-                        next_date += relativedelta(day=line.day_of_the_month, months=months_delta)
-                elif line.option == 'after_invoice_month':
-                    next_first_date = next_date + relativedelta(day=1, months=1)  # Getting 1st of next month
+                        months_delta = (
+                            (line.day_of_the_month < next_date.day) and 1 or 0
+                        )
+                        next_date += relativedelta(
+                            day=line.day_of_the_month, months=months_delta
+                        )
+                elif line.option == "after_invoice_month":
+                    next_first_date = next_date + relativedelta(
+                        day=1, months=1
+                    )  # Getting 1st of next month
                     next_date = next_first_date + relativedelta(days=line.days - 1)
-                elif line.option == 'day_following_month':
+                elif line.option == "day_following_month":
                     next_date += relativedelta(day=line.days, months=1)
-                elif line.option == 'day_current_month':
+                elif line.option == "day_current_month":
                     next_date += relativedelta(day=line.days, months=0)
-                result.append((fields.Date.to_string(next_date), amt, line.days))
+
+                result.append(
+                    (fields.Date.to_string(next_date), amt, line.days, amt_without_taxes)
+                )
+
                 amount -= amt
+                amount_without_taxes -= amt_without_taxes
+
         if result is not False:
             amount = sum(amt[1] for amt in result)
+            amount_without_taxes = sum(amt_without_taxes[3] for amt_without_taxes in result)
+
         dist = currency.round(value - amount)
+        dist_without_taxes = currency.round(value_without_taxes - amount_without_taxes)
+
         if dist:
             last_date = result and result[-1][0] or fields.Date.today()
             days = result and result[-1][2] or 0
             result.append((last_date, dist, days))
+
+        if dist_without_taxes:
+            last_date = result and result[-1][0] or fields.Date.today()
+            days = result and result[-1][2] or 0
+            result.append((last_date, dist_without_taxes, days))
+
         return result
 
     @api.multi
