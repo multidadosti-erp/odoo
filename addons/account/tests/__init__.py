fd3253b9ae4c6e5f6e8f8b76460cabb207e42673
Augusto Costa <gustotc@gmail.com>
2024-08-28 14:20:56 -0300

    Remove Testes de Reconciliação do Core módulo 'account'
    - Testes removidos pelo uso diferenciado no MultiERP.

--- addons/account/tests/__init__.py
+++ addons/account/tests/__init__.py
@@ -7,7 +7,7 @@ from . import test_account_supplier_invoice
 from . import test_account_validate_account_move
 from . import test_account_invoice_rounding
 from . import test_account_move_rounding
-from . import test_bank_statement_reconciliation
+# from . import test_bank_statement_reconciliation
 from . import test_fiscal_position
 # from . import test_invoice_onchange
 # from . import test_reconciliation_widget
@@ -22,4 +22,4 @@ from . import test_templates_consistency
 from . import test_account_fiscal_year
 from . import test_account_all_l10n
 # from . import test_reconciliation_matching_rules
-from . import test_reconciliation_heavy_load
+# from . import test_reconciliation_heavy_load
