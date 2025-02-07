c3073716add8654544726e4a5977b7f4633313f1
Fernando Saeta <fernando.r.saeta@gmail.com>
2025-02-07 10:28:51 -0300

    Correção no método da controller (formato incorreto de valores)
    - Os valores processados pelo método estava retornando o recordset
      res.users() para o campo `values['user_id']`, o qual espera-se
      que receba um bool ou int.
    
    - Foi percebido o erro aplicando testes e comparando na função
      write, que o values['user_id'] estava retornando Log de Warning.

--- addons/website_sale/controllers/main.py
+++ addons/website_sale/controllers/main.py
@@ -607,8 +607,8 @@ class WebsiteSale(ProductConfiguratorController):
                     _logger.debug("website_sale postprocess: %s value has been dropped (empty or not writable)" % k)
 
         new_values['customer'] = True
-        new_values['team_id'] = request.website.salesteam_id and request.website.salesteam_id.id
-        new_values['user_id'] = request.website.salesperson_id and request.website.salesperson_id.id
+        new_values['team_id'] = request.website.salesteam_id.id
+        new_values['user_id'] = request.website.salesperson_id.id
 
         if request.website.specific_user_account:
             new_values['website_id'] = request.website.id
