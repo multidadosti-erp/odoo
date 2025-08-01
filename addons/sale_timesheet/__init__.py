5279bc192d96f7a8f8318de9418f456bb0c5f610
Fernando Saeta <fernando.r.saeta@gmail.com>
2025-08-01 14:52:48 -0300

    Remove conteúdo do módulo "sale_timesheet"
    - O conteúdo não estava sendo utilizado, causando mais complexidade
      e peso no carregamento das models / banco de dados.
    
    - O conteúdo do módulo foi "sobrescrito" pelo módulo br_sale_project,
      para uma finalidade parecida, mas diferente, mantendo o módulo
      sale_timesheet obsoleto.

--- addons/sale_timesheet/__init__.py
+++ addons/sale_timesheet/__init__.py
@@ -6,7 +6,7 @@ from odoo import api, SUPERUSER_ID
 from . import models
 from . import wizard
 from . import controllers
-from . import report
+# from . import report
 
 def uninstall_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
