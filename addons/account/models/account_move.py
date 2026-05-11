213989f7b78bea5af9550ba51de0e954bd622930
Augusto Costa <gustotc@gmail.com>
2026-05-11 11:43:42 -0300

    Ajusta '_update_check' módulo 'account'
    - Adiciona verificação de variável de contexto: 'ignore_state_update_check'
      Para liberar a alteração da Conta mesmo se o State for diferente de
      'draft' para atender uma demanda do MultiERP.

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -1321,10 +1321,14 @@ class AccountMoveLine(models.Model):
     @api.multi
     def _update_check(self):
         """ Raise Warning to cause rollback if the move is posted, some entries are reconciled or the move is older than the lock date"""
+
+        # Adiciona o ignore_state_update_check para permitir a atualização de campos não legais em lançamentos contábeis postados.
+        ignore_state = self.env.context.get('ignore_state_update_check', False)
+
         move_ids = set()
         for line in self:
             err_msg = _('Move name (id): %s (%s)') % (line.move_id.name, str(line.move_id.id))
-            if line.move_id.state != 'draft':
+            if line.move_id.state != 'draft' and not ignore_state:
                 raise UserError(_('You cannot do this modification on a posted journal entry, you can just change some non legal fields. You must revert the journal entry to cancel it.\n%s.') % err_msg)
             if line.reconciled and not (line.debit == 0 and line.credit == 0):
                 raise UserError(_('You cannot do this modification on a reconciled entry. You can just change some non legal fields or you must unreconcile first.\n%s.') % err_msg)
